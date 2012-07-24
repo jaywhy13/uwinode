e169e8df15272f50b76c2f4505c73ab2915309e0
Jean-Mark Wright <jeanmark.wright@gmail.com>
2012-07-23 22:11:47 -0500

    Added search urls for data and layers so we could put in the latest layers/maps dictionary

--- uwinode/maps/urls.py
+++ uwinode/maps/urls.py
@@ -5,8 +5,10 @@ import uwinode.maps.urls
 
 datapatterns = patterns('uwinode.maps.views',
   url(r'^search/latest/$', 'metadata_search_latest', name='data_search_latest'),
+  url(r'^search/?$', 'show_search_page', name='data_search'),
 )
 
 urlpatterns = patterns('uwinode.maps.views',
     url(r'^search/latest?$', 'maps_search_latest', name='maps_search_latest'),
+    url(r'^search/?$', 'maps_search_page', name='maps_search'),
 )
